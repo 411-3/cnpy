587e7a9a0575c0a124ff07d61feecabd5d559cb2
411-3 <2250611484@qq.com>
2017-02-20 12:02:12 +0800

    create cnpy

---
+++ build/CMakeFiles/cnpy.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xduser/LiHuan/fcn/cnpy-master/cnpy.cpp" "/home/xduser/LiHuan/fcn/cnpy-master/build/CMakeFiles/cnpy.dir/cnpy.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/CMakeFiles/example1.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xduser/LiHuan/fcn/cnpy-master/example1.cpp" "/home/xduser/LiHuan/fcn/cnpy-master/build/CMakeFiles/example1.dir/example1.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/xduser/LiHuan/fcn/cnpy-master/build/CMakeFiles/cnpy.dir/DependInfo.cmake"
+  )
